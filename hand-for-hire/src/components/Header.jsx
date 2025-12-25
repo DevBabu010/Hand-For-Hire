00e25fa9417951e21efe5407795202c131274864
Devbabu010 <neoshail010@gmail.com>
2025-12-26 01:28:24 +0530

    2

---
+++ hand-for-hire/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Header.css";
+import logo from "../assets/hfhLogo.png";
+
+const Header = () => {
+    return (
+    <div className='headerDiv'>
+        <div><img src={logo} alt="logo" /></div>
+        <div>Hand For Hire</div>
+        
+
+
+
+        
+    </div>
+    )
+}
+
+export default Header
